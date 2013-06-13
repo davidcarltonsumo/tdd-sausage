1fb5c40f0edb5d15713ad9e051160d5e863c72c4
David Carlton <carlton@sumologic.com>
2013-06-13 14:04:59 -0700

    Install multiple shards.

--- src/test/scala/ShardDiskCacheTest.scala
+++ src/test/scala/ShardDiskCacheTest.scala
@@ -4,24 +4,24 @@ import org.mockito.Mockito._
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.WordSpec
+import org.scalatest.{BeforeAndAfterEach, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
-class ShardDiskCacheTest extends WordSpec with ShouldMatchers with MockitoSugar {
+class ShardDiskCacheTest extends WordSpec with ShouldMatchers with MockitoSugar
+with BeforeAndAfterEach {
+  var downloader: Downloader = _
+  var cache: ShardDiskCache = _
+
   "A ShardDiskCache" should {
     "install a shard and return the resulting File" in {
-      val downloader = mock[Downloader]
       when(downloader.download("path")).thenReturn(new File("installed-path"))
-      val cache = new ShardDiskCache(downloader)
       val shard = Shard("name", "path")
 
       cache.get(shard) should equal (new File("installed-path"))
     }
 
     "not re-install a shard that is already installed" in {
-      val downloader = mock[Downloader]
       when(downloader.download("path")).thenReturn(new File("installed-path"))
-      val cache = new ShardDiskCache(downloader)
       val shard = Shard("name", "path")
 
       cache.get(shard)
@@ -30,7 +30,16 @@ class ShardDiskCacheTest extends WordSpec with ShouldMatchers with MockitoSugar 
       verify(downloader, times(1)).download("path")
     }
 
-    // Install a bunch of shards.
+    "install multiple shards" in {
+      when(downloader.download("path-1")).thenReturn(new File("installed-path-1"))
+      when(downloader.download("path-2")).thenReturn(new File("installed-path-2"))
+      when(downloader.download("path-3")).thenReturn(new File("installed-path-3"))
+
+      cache.get(Shard("name-1", "path-1")) should equal (new File("installed-path-1"))
+      cache.get(Shard("name-2", "path-2")) should equal (new File("installed-path-2"))
+      cache.get(Shard("name-3", "path-3")) should equal (new File("installed-path-3"))
+    }
+
     // Install multiple shards in parallel.
     // Error cases when installing shards?
     // Not download a shard that's in the process of being downloaded.
@@ -38,5 +47,13 @@ class ShardDiskCacheTest extends WordSpec with ShouldMatchers with MockitoSugar 
     // Evict old shards if the disk is full.
     // Initialize the cache from disk on startup.
     // Not get confused if the process gets terminated while we're downloading.
+    // Questions:
+    // * Should Shard store the name?
+    // * Should Downloader take the name as well as the path?
+  }
+
+  override protected def beforeEach() {
+    downloader = mock[Downloader]
+    cache = new ShardDiskCache(downloader)
   }
 }
