d9ef9795b4d55343549e8312208ad52f8ee87f02
David Carlton <carlton@sumologic.com>
2013-06-13 16:01:15 -0700

    Add a framework for doing multiple downloads in parallel.

--- src/test/scala/ShardDiskCacheTest.scala
+++ src/test/scala/ShardDiskCacheTest.scala
@@ -1,15 +1,14 @@
 import java.io.File
 import org.junit.runner.RunWith
-import org.mockito.Mockito._
+import org.scalatest.concurrent.Eventually
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.mock.MockitoSugar
 import org.scalatest.{BeforeAndAfterEach, WordSpec}
 import scala.collection.mutable
 
 @RunWith(classOf[JUnitRunner])
-class ShardDiskCacheTest extends WordSpec with ShouldMatchers with MockitoSugar
-with BeforeAndAfterEach {
+class ShardDiskCacheTest extends WordSpec with ShouldMatchers
+with BeforeAndAfterEach with Eventually {
   var downloader: StubDownloader = _
   var cache: ShardDiskCache = _
 
@@ -24,7 +23,7 @@ with BeforeAndAfterEach {
       cache.get(shard)
       cache.get(shard) should equal (new File("installed-path"))
 
-      downloader.downloadCounts("path") should equal (1)
+      downloader.downloadCount("path") should equal (1)
     }
 
     "install multiple shards" in {
@@ -34,13 +33,31 @@ with BeforeAndAfterEach {
     }
 
     "install multiple shards in parallel" in {
-      // Fire off three requests.
-      // Tell the downloader not to answer the requests for a bit.
-      // Then let it answer the requests.
-      // Make sure they show up eventually.
+      downloader.delay("path-1")
+      downloader.delay("path-2")
+      downloader.delay("path-3")
+
+      val requester1 = backgroundRequester(1)
+      val requester2 = backgroundRequester(2)
+      val requester3 = backgroundRequester(3)
+
+      eventually { requester1 should be ('started) }
+      eventually { requester2 should be ('started) }
+      eventually { requester3 should be ('started) }
+
+      downloader.resume("path-1")
+      downloader.resume("path-2")
+      downloader.resume("path-3")
+
+      eventually { requester1 should be ('done) }
+      eventually { requester2 should be ('done) }
+      eventually { requester3 should be ('done) }
+
+      requester1.result should equal (new File("installed-path-1"))
+      requester2.result should equal (new File("installed-path-2"))
+      requester3.result should equal (new File("installed-path-3"))
     }
 
-    // Install multiple shards in parallel.
     // Not download a shard that's in the process of being downloaded.
     // Bound the number of simultaneous downloads.
     // Error cases when installing shards?
@@ -53,12 +70,52 @@ with BeforeAndAfterEach {
   }
 
   class StubDownloader extends Downloader {
-    val downloadCounts = mutable.Map[String, Int]()
+    val downloadCount = mutable.Map[String, Int]()
+    val delayedDownloads = mutable.Set[String]()
 
     def download(path: String): File = {
-      downloadCounts(path) = downloadCounts.getOrElse(path, 0) + 1
+      waitForPermissionToDownload(path)
+      downloadCount(path) = downloadCount.getOrElse(path, 0) + 1
       new File("installed-" + path)
     }
+
+    def waitForPermissionToDownload(path: String) {
+      while (delayedDownloads.contains(path)) {
+        Thread.sleep(10)
+      }
+    }
+
+    def delay(path: String) {
+      delayedDownloads += path
+    }
+
+    def resume(path: String) {
+      delayedDownloads -= path
+    }
+  }
+
+  class BackgroundRequester(shard: Shard) {
+    private var started = false
+    private var resultOption: Option[File] = None
+
+    def start() {
+      new Thread {
+        override def run() {
+          started = true
+          resultOption = Some(cache.get(shard))
+        }
+      }.start()
+    }
+
+    def isStarted: Boolean = started
+    def isDone: Boolean = resultOption.isDefined
+    def result: File = resultOption.get
+  }
+
+  def backgroundRequester(i: Int): BackgroundRequester = {
+    val requester = new BackgroundRequester(Shard(f"name-$i", f"path-$i"))
+    requester.start()
+    requester
   }
 
   override protected def beforeEach() {
