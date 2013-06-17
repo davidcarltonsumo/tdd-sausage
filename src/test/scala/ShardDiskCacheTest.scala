c0957f226b74ad0042ac0c1e348fa5a02faa4013
David Carlton <carlton@sumologic.com>
2013-06-17 10:58:12 -0700

    Add initial quetions, and stick in pom stuff for mockito.

--- src/test/scala/ShardDiskCacheTest.scala
+++ src/test/scala/ShardDiskCacheTest.scala
@@ -9,5 +9,16 @@ class ShardDiskCacheTest extends WordSpec with ShouldMatchers {
     "do something" in {
       1 should equal (3)
     }
+
+    // Install a shard.
+    // Not re-install a shard that is already installed.
+    // Install a bunch of shards.
+    // Install multiple shards in parallel.
+    // Not download a shard that's in the process of being downloaded.
+    // Bound the number of simultaneous downloads.
+    // Evict old shards if the disk is full.
+    // Initialize the cache from disk on startup.
+    // Not get confused if the process gets terminated while we're downloading.
+    // Provide a prefetch mechanism where we say that we'll want something in the future?
   }
 }
